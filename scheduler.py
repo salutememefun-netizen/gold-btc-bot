9b4b4a41d530c0af704e2f7da50d162c15f74683
salutememefun-netizen <salutememefun@gmail.com>
2026-08-25 00:22:56 +0800

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,11 @@
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from helpers import analyze_gold_btc
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Simpan senarai chat_id yang subscribe
+subscribed_users = set()
+
+def add_subscriber(chat
